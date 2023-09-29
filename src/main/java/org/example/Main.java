f2da205ab6aacc6c9756de5b2c7425a9eeb23aa2
carolangel19 <102700849+CarolAngel19@users.noreply.github.com>
2023-09-28 20:15:48 -0500

    Commit 2 Definición modelo de datos primitivos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        String productName = "Papas Super Ricas Todo Rico BBQ";
+        float productPrice = 5.900f;
+        int quantityStock = 10;
+        boolean isAvailable = true;
+        String category = "Pasabocas";
+        String tags = "Papas Fritas y Paquetes";
+        String description = "Bolsa x 250 gr";
+
         System.out.println("Mi Tienda de Barrio App");
+
+        System.out.println("Nombre del producto: " + productName);
+        System.out.println("Precio del producto: " + productPrice);
+        System.out.println("Cantidad del producto: " + quantityStock);
+        System.out.println("Disponible: " + isAvailable);
+        System.out.println("Categoria del producto: " + category);
+        System.out.println("Etiquetas del producto: " + tags);
+        System.out.println("Descripcion del producto: " + description);
     }
 }
